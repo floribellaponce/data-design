0094dd77f95e0b9a69241ad6191be977491bbc20
Floribella Ponce <fponce2@cnm.edu>
2019-10-09 15:29:19 -0600

    updated with new file links

--- epic/index.php
+++ epic/index.php
@@ -5,19 +5,14 @@
 	<title>Directory</title>
 	<link rel="stylesheet" href="style.css">
 </head>
-<body bgcolor="#d8bfd8">
+<body bgcolor="#afeeee">
 <div align="center">
 	<header><strong>Data Design Project</strong></header>
-	<div class="row">
-		<div class="column">
-			<header><strong>PERSONA (click image)</strong></header>
-			<button><a href="persona.php"><img src="images/bella.jpg" alt="user image" style="width:200px;height=300px;"></a></button>
+	<div>
+		<button><a href="persona.php">PERSONA</a></button>
+		<button><a href="user-story.php">USER STORY</a></button>
+		<button><a href="use-case.php">USE CASE</a></button>
 		</div>
-		<div>
-			<header><strong>USER STORY/CASE (click image)</strong></header>
-			<button><a href="user-story-use-case.php"><img src="images/54230848_2275244912744921_8381043613403971584_o.jpg" alt="dogs" style="width:200px;height=300px;"></a></button>
-		</div>
-	</div>
 </div>
 </body>
 </html>
