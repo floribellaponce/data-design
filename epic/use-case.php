cf4e7c2ff421bde40ff17c3f1ea452d7224ad4d7
Floribella Ponce <fponce2@cnm.edu>
2019-10-09 15:23:10 -0600

    use case and user story revision

---
+++ epic/use-case.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Use Case</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#afeeee">
+<div align="center">
+	<header><strong>Use Case</strong></header>
+</div>
+<p>
+	<strong>Title: </strong>Creating a dog profile
+</p>
+<p>
+	<strong>Description: </strong> User will have the ability to create a dog profile with dogs information on website
+</p>
+<p>
+	<strong> Name of user: </strong> Alexander Gonzalez & Paramedic
+</p>
+<p>
+	<strong> Users role: </strong> Advance Paramedic
+</p>
+<p>
+	<strong>Usage preconditions: </strong> User has never accessed website before and needs to create a profile for his dog(s).
+</p>
+<p>
+	<strong>Usage postconditions: </strong> User has successfuly created a dog profile for his dog(s) and has the ability to share the webpage.
+</p>
+<div>
+	<button><a href="index.php">Go back</a></button>
+</div>
+</body>
+</html>
---
+++ epic/user-story.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Story and Use Case</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#afeeee">
+<div align="center">
+	<header><strong>User Story</strong></header>
+	<p>As a user I would like to create a profile for a lost/missing dog which will contain the dogs information.</p>
+</div>
+<div>
+	<button><a href="index.php">Go back</a></button>
+</div>
+</body>
+</html>
