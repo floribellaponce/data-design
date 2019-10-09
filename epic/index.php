971e17ff34ad1d44fa342cae9f3a20f0bd9946aa
Floribella Ponce <fponce2@cnm.edu>
2019-10-09 02:33:59 -0600

    phase 1 files for persona, user story and use case

---
+++ epic/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Directory</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div align="center">
+	<header><strong>Data Design Project</strong></header>
+	<div class="row">
+		<div class="column">
+			<header><strong>PERSONA</strong></header>
+			<button><a href="persona.php"><img src="images/bella.jpg" style="width:200px;height=300px;"></a></button>
+		</div>
+		<div>
+			<header><strong>USER STORY/CASE</strong></header>
+			<button><a href="user-story-use-case.php"><img src="images/54230848_2275244912744921_8381043613403971584_o.jpg" style="width:200px;height=300px;"></a></button>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ epic/persona.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Persona</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div align="center">
+	<header><strong>Persona</strong></header>
+</div>
+<div class="row">
+	<div class="column">
+		<header><strong>User</strong></header>
+		<p>
+			Floribella is 27 year old who is currently attending a development bootcamp. When she is not attending the bootcamp she is
+			communiting to work via bicycle or bus, cooking, taking care of her sister, and playing with her four dogs. The mayority of her
+			time is spent with her dogs since taking care of 4 dogs requires a lot of work and attention. Floribella has been a dog lover ever since she could
+			remember. She is the go to person for when her friends/family have lost their dog or have found a dog. She has also encountered lost dogs herself
+			in which she takes into her house until the owner appears. But due to her busy days she is unable to help every dog she encounters.
+		</p>
+		<header><strong>Frustrations</strong></header>
+		<ul>
+			<li>Busy schedule</li>
+			<li>Inefficient time consuming tools</li>
+			<li>Unable to access desktop at all times</li>
+			<li> Unable to help every dog</li>
+		</ul>
+		<header><strong>Needs</strong></header>
+		<ul>
+			<li>Ability to access sites on mobile devices</li>
+			<li>Ability to create a profile quickly</li>
+			<li>Ability to share via link</li>
+			<li>Ability to export into pdf</li>
+		</ul>
+		<p>
+			<em>Floribella has experienced scares due to her dogs gone missing or a friends/family's dog having gone missing.
+				Because of this Floribella has decided to start using our site in hopes of an efficient way to help her and her pets.</em>
+		</p>
+	</div>
+	<div class="column">
+		<img src="images/bella.jpg" atl="user image" style="width:400px;height=500px;">
+	</div>
+</div>
+</html>
