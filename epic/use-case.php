d329f9d48d955da4fe0045450f8784cd7ca63520
Floribella Ponce <fponce2@cnm.edu>
2019-10-09 16:15:21 -0600

    added interative flow

--- epic/use-case.php
+++ epic/use-case.php
@@ -27,6 +27,15 @@
 <p>
 	<strong>Usage postconditions: </strong> User has successfuly created a dog profile for his dog(s) and has the ability to share the webpage.
 </p>
+<p>
+	<strong>Interactive Flow</strong>
+<ol>
+	<li><strong>Open website </strong>- website should display option to create a dog profile</li>
+	<li><strong>Select on button to create dog profile </strong>- dog profile form should appear</li>
+	<li><strong>Enter required information on dog profile form </strong>- form will have required information in open field boxes, dropdown lists, and radio buttons</li>
+	<li><strong>Save dog profile </strong>- profile should successfully save and page will redirect to dogs profile </li>
+</ol>
+</p>
 <div>
 	<button><a href="index.php">Go back</a></button>
 </div>
