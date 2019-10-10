92f1cab6046837d8d2e264856c268ced27bbc40f
Floribella Ponce <fponce2@cnm.edu>
2019-10-10 16:41:21 -0600

    added keys

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,51 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>ERD-DataDesign-Phase1</title>
 	<meta charset="utf-8"/>
 </head>
 <body>
+<h1 align="center">Conceptual Model</h1>
+<h2>Entities & Attributes</h2>
+<h3>Dog</h3>
+<ul>
+	<li>dogID (primary key)</li>
+	<li>dogName</li>
+	<li>dogBreed</li>
+	<li>dogColor</li>
+	<li>dogSize</li>
+	<li>dogAge</li>
+	<li>dogImage</li>
+	<li>dogExtraComments</li>
+	<li>ownerID (foreign key)</li>
+</ul>
+<h3>Owner</h3>
+<ul>
+	<li>ownerID (primary key)</li>
+	<li>ownerName</li>
+	<li>ownerHash (password)</li>
+	<li>ownerAddress</li>
+	<li>ownerPhoneNumber</li>
+	<li>ownerEmail</li>
+</ul>
+<h3>Relationships</h3>
+<ul>
+	<li>One dog can have only one owner (1 to 1) </li>
+	<li>One owner can have one or many dogs (1 to n) </li>
+</ul>
+<h1 align="center">Entity Relationship Diagram</h1>
 <div align="center">
-	<header><strong>Conceptual Model</strong></header>
-</div>
-<header><strong>Entities & Attributes</strong></header>
-<p>
-<div>
-	<header><strong>Dog</strong></header>
-	<ul>
-		<li>dogID</li>
-		<li>dogName</li>
-		<li>dogBreed</li>
-		<li>dogColor</li>
-		<li>dogSize</li>
-		<li>dogAge</li>
-		<li>dogImage</li>
-		<li>dogExtraComments</li>
-	</ul>
-</div>
-<div>
-	<header><strong>Owner</strong></header>
-	<ul>
-		<li>ownerID</li>
-		<li>ownerName</li>
-		<li>ownerAddress</li>
-		<li>ownerPhoneNumber</li>
-		<li>ownerEmail</li>
-	</ul>
-</div>
-<div>
-	<header><strong>Relationships</strong></header>
-	<ul>
-		<li>One dog can have only one owner (1 to 1) </li>
-		<li>One owner can have one or many dogs (1 to n) </li>
-	</ul>
-</div>
-</p>
-<div align="center">
-	<header><strong>Entity Realtionship Diagram</strong></header>
-</div>
-<div align="center">
-	<img src="images/ERD-DataDesign-Phase1.png" alt="ERD" style="width:1200px;height:550px";>
+	<img src="images/ERD-DataDesign-Phase1.png" alt="ERD" style="width:1200px;height:550px;">
 </div>
 <div>
 	<button><a href="index.php">Go back</a></button>
