08d74e22f3c8f1ea382575e69bd46defbdcc8e20
Floribella Ponce <fponce2@cnm.edu>
2019-10-15 16:55:11 -0600

    updated ERD

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,6 +10,7 @@
 <h3>Dog</h3>
 <ul>
 	<li>dogID (primary key)</li>
+	<li>dogUserID (foreign key)</li>
 	<li>dogName</li>
 	<li>dogBreed</li>
 	<li>dogColor</li>
@@ -17,25 +18,32 @@
 	<li>dogAge</li>
 	<li>dogImage</li>
 	<li>dogExtraComments</li>
-	<li>ownerID (foreign key)</li>
 </ul>
-<h3>Owner</h3>
+<h3>User</h3>
 <ul>
-	<li>ownerID (primary key)</li>
-	<li>ownerName</li>
-	<li>ownerHash (password)</li>
-	<li>ownerAddress</li>
-	<li>ownerPhoneNumber</li>
-	<li>ownerEmail</li>
+	<li>userID (primary key)</li>
+	<li>userName</li>
+	<li>userHash (password)</li>
+	<li>userAddress</li>
+	<li>userPhoneNumber</li>
+	<li>userEmail</li>
+</ul>
+<h3>Comment</h3>
+<ul>
+	<li>commentID (primary key) </li>
+	<li>commentUserID (foreign key)</li>
+	<li>commentDogID (foreign key)</li>
+	<li>commentContent</li>
 </ul>
 <h3>Relationships</h3>
 <ul>
-	<li>One dog can have only one owner (1 to 1) </li>
-	<li>One owner can have one or many dogs (1 to n) </li>
+	<li>One user to one or many dogs (1 to n)</li>
+	<li>One user can leave none or many comments(1 to n)</li>
+	<li>One dog can have none or many comments(1 to n)</li>
 </ul>
 <h1 align="center">Entity Relationship Diagram</h1>
 <div align="center">
-	<img src="images/ERD-DataDesign-Phase2%20(1).png" alt="ERD" style="width:1200px;height:550px;">
+	<img src="images/ERD-DataDesign-Phase2%20(2).png" alt="ERD" style="width:1200px;height:550px;">
 </div>
 <div>
 	<button><a href="index.php">Go back</a></button>
